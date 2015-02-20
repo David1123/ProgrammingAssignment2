af99ba2132346c76321ea21382bb941bd5ae0ce0
David1123 <david@ljf.com.auget config --listgit config --global user.email david@ljf.com.au>
2015-02-20 13:24:31 +1030

    Completed Assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The functions take a matrix, and then solve for its inverse,
+## retrieving a cached value if called multiple times
 
-## Write a short comment describing this function
+## Function creates a 'matrix' that can be evaluated by cacheSolve
+## to quickly give the inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setsolve <- function(solve) m <<- solve
+  getsolve <- function() m
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
+## Function returns the inverse of a matrix created by makeCacheMatrix,
+## either by calculating it, or by returning the cached value
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    m <- x$getsolve()
+    if(!is.null(m)) {
+        message("Cached value for you!")
+        return(m)
+    }
+    data <- x$get()
+    m <- solve(data, ...)
+    x$setsolve(m)
+    m    
+    
 }
